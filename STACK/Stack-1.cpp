705ef576f7df858132863ff0844cc09ffdaf93ae
samir1120k <syraverma1120s@gmail.com>
2024-08-04 10:03:51 +0530

    add stack file

---
+++ STACK/Stack-1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+    /*
+    // creation of stack
+    stack<int> s;
+    // push operation
+    s.push(3);
+    s.push(8);
+    s.push(9);
+    // pop operations
+    s.pop();
+    s.pop();
+
+    cout << "top element is " << s.top() << endl;
+    // checking stack is empty or not
+    if (s.empty())
+    {
+        cout << "stack is empty" << endl;
+    }
+    else
+    {
+        cout << "stack is not empty" << endl;
+    }
+    cout << "size of stack is " << s.size() << endl;
+*/
+    return 0;
+}
